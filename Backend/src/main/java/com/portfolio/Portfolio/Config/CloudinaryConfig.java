64a7f4121793128a2a32df4ff478c597fff123d1
subratpandeyy <12subratpandey@gmail.com>
2025-10-20 19:55:26 +0530

    config error

--- Backend/src/main/java/com/portfolio/Portfolio/Config/CloudinaryConfig.java
+++ Backend/src/main/java/com/portfolio/Portfolio/Config/CloudinaryConfig.java
@@ -2,18 +2,29 @@ package com.portfolio.Portfolio.Config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dad8dnpev",
-                "api_key", "566287954292653",
-                "api_secret", "tL0Ix4DsNPnl_Y7beVZMc_1Mdjc"
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
+                "secure", true
         ));
     }
 }
